73206ccae56d282a89a22bd98f5524423dba58cf
Deju Adejo <ojomideju2003@gmail.com>
2025-04-29 10:24:01 +0100

    added voting functionality

--- server/controllers/voteController.js
+++ server/controllers/voteController.js
@@ -5,8 +5,42 @@ export const castVote = async (req, res) => {
   const { voteData, studentId } = req.body;
 
   try {
-    console.log("Received Data:", voteData);
-    console.log("Student ID:", studentId);
+    // console.log("Vote data:", voteData);
+    // console.log("Student ID:", studentId);
+
+    // Start a transaction
+    await db.query("BEGIN");
+
+    // Check if the student has already voted
+    const checkVote = await db.query("SELECT * FROM votes WHERE voter = $1", [
+      studentId,
+    ]);
+
+    if (checkVote.rows.length > 0) {
+      return res.status(400).json({ error: "User has already voted!" });
+    }
+
+    // Insert user votes into the votes table
+    for (const position in voteData) {
+      const candidateId = voteData[position];
+
+      // Insert into votes table
+      await db.query(
+        "INSERT INTO votes (voter, candidate_id) VALUES ($1, $2)",
+        [studentId, candidateId]
+      );
+
+      // Increment the vote count for the candidate
+      await db.query(
+        "UPDATE candidates SET votes_count = votes_count + 1 WHERE id = $1",
+        [candidateId]
+      );
+    }
+
+    // Commit transaction
+    await db.query("COMMIT");
+
+    res.status(201).json({ message: "Votes submitted successfully!" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Vote failed" });
