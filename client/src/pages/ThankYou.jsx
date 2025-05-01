5a347b832a16c2bec64d9770d3210d7ba9ebb41f
Deju Adejo <ojomideju2003@gmail.com>
2025-05-01 21:08:30 +0100

    updated thank you for voting  page

--- client/src/pages/ThankYou.jsx
+++ client/src/pages/ThankYou.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ThankYou = () => {
-  return <div className="">Thanks for Voting</div>;
+  return (
+    <div className="flex flex-col gap-8 items-center justify-center p-4">
+      <img
+        src="/assets/thank-you.png"
+        alt="thank-you"
+        className="max-h-[50vh] w-full object-cover"
+      />
+      <Link
+        to="/"
+        className="inline-block mt-8 px-6 py-3 text-white font-bold rounded-xl bg-gradient-to-b from-[var(--color-special)] to-[var(--color-accent)] hover:scale-105 transition-transform duration-300 shadow-lg"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
 };
 
 export default ThankYou;
